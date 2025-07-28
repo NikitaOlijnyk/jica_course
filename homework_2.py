becd9bda0195c4140fe762f272d0ac350d822c7a
Mykyta Oliinyk <144736223+NikitaOlijnyk@users.noreply.github.com>
2025-07-28 11:32:48 +0200

    Delete homework_2.py

--- homework_2.py
+++
@@ -1,5 +0,0 @@
-def formatuj(tekst:str):
-    tekst = tekst.strip().lower().replace("python", "PYTHON")
-    return tekst
-
-print(formatuj("     hniashdasjdhsa ashdjhjds  s  s    ASHAJHSKJAHJKSAJKSH PYTHON python    dskfhsjdfhkjshfkskj asdsadas          "))
