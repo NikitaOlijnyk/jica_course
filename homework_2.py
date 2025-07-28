f3deb480c3d950c93816eb87c631a5497c2d413a
Mykyta Oliinyk <mykytaoliinyk@Mykytas-MacBook-Air.local>
2025-07-28 10:34:15 +0200

    Your commit message

---
+++ homework_2.py
@@ -0,0 +1,5 @@
+def formatuj(tekst:str):
+    tekst = tekst.strip().lower().replace("python", "PYTHON")
+    return tekst
+
+print(formatuj("     hniashdasjdhsa ashdjhjds  s  s    ASHAJHSKJAHJKSAJKSH PYTHON python    dskfhsjdfhkjshfkskj asdsadas          "))
